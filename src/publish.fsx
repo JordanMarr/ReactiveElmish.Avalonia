b1a670d7b75b6298b0ccb586f2e124471149ed84
Jordan Marr <jmarr@microdesk.com>
2023-10-26 11:20:09 -0400

    Replaced FAKE env dep with Fun.Build helper fn.

--- src/publish.fsx
+++ src/publish.fsx
@@ -1,10 +1,8 @@
 #r "nuget: Fun.Build, 1.0.2"
-#r "nuget: Fake.Core.Environment, 5.22.0"
 
 open System.IO
 open System.Xml.Linq
 open Fun.Build
-open Fake.Core
 
 let src = __SOURCE_DIRECTORY__
 
@@ -16,18 +14,17 @@ pipeline "Publish" {
     }
     
     stage "Publish Elmish.Avalonia" {
-        run (fun _ -> 
+        run (fun ctx ->             
             let version = 
                 let project = (FileInfo $"{src}/Elmish.Avalonia/Elmish.Avalonia.fsproj")
-                let doc = XDocument.Load(project.FullName)
-                match doc.Descendants("Version") |> Seq.tryHead with
+                match XDocument.Load(project.FullName).Descendants("Version") |> Seq.tryHead with
                 | Some versionElement -> versionElement.Value
                 | None -> failwith $"Could not find a <Version> element in '{project.Name}'."
-
-            let nugetKey =
-                match Environment.environVarOrNone "ELMISH_AVALONIA_NUGET_KEY" with
-                | Some nugetKey -> nugetKey
-                | None -> failwith "The NuGet API key must be set in an 'ELMISH_AVALONIA_NUGET_KEY' environmental variable"
+            
+            let nugetKey = 
+                match ctx.TryGetEnvVar "ELMISH_AVALONIA_NUGET_KEY" with
+                | ValueSome nugetKey -> nugetKey
+                | ValueNone -> failwith "The NuGet API key must be set in an 'ELMISH_AVALONIA_NUGET_KEY' environmental variable"
             
             $"dotnet nuget push \"{src}/Elmish.Avalonia/bin/Release/Elmish.Avalonia.{version}.nupkg\" -s nuget.org -k {nugetKey} --skip-duplicate"
         )
