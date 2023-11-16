d8b4f3da516b5e3420fed5db8f12a63b0e2fcfef
Jordan Marr <jmarr@microdesk.com>
2023-11-16 18:35:21 -0500

    Reverted MainViewModel back to ReactiveElmishViewModel<Model, Msg>

--- src/Samples/AvaloniaExample/ViewModels/MainViewModel.fs
+++ src/Samples/AvaloniaExample/ViewModels/MainViewModel.fs
@@ -5,21 +5,14 @@ open Elmish
 open App
 
 type MainViewModel() as this =
-    inherit ReactiveViewModel()
+    inherit ReactiveElmishViewModel<Model, Msg>(App.init())
 
-    let app = 
-        Program.mkAvaloniaSimple App.init App.update
-        |> Program.withErrorHandler (fun (_, ex) -> printfn "Error: %s" ex.Message)
-        |> Program.withConsoleTrace
-        |> Program.mkStore
-        |> this.AddDisposable
-
-    let counterVM = lazy (new CounterViewModel(app) : ReactiveUI.ReactiveObject)
-    let aboutVM = lazy (new AboutViewModel(app))
-    let chartVM = lazy (new ChartViewModel(app))
+    let counterVM = lazy (new CounterViewModel(this) : ReactiveUI.ReactiveObject)
+    let aboutVM = lazy (new AboutViewModel(this))
+    let chartVM = lazy (new ChartViewModel(this))
     let filePickerVM = lazy (new FilePickerViewModel())
 
-    member this.ContentVM = this.Bind (app, fun m -> 
+    member this.ContentVM = this.Bind (fun m -> 
         match m.View with
         | CounterView -> counterVM.Value
         | AboutView -> aboutVM.Value
@@ -27,9 +20,15 @@ type MainViewModel() as this =
         | FilePickerView -> filePickerVM.Value
     )
     
-    member this.ShowChart() = app.Dispatch (SetView ChartView)
-    member this.ShowCounter() = app.Dispatch (SetView CounterView)
-    member this.ShowAbout() = app.Dispatch (SetView AboutView)
-    member this.ShowFilePicker() = app.Dispatch (SetView FilePickerView)
+    member this.ShowChart() = this.Dispatch (SetView ChartView)
+    member this.ShowCounter() = this.Dispatch (SetView CounterView)
+    member this.ShowAbout() = this.Dispatch (SetView AboutView)
+    member this.ShowFilePicker() = this.Dispatch (SetView FilePickerView)
+
+    override this.StartElmishLoop(view: Avalonia.Controls.Control) = 
+        Program.mkAvaloniaSimple App.init App.update
+        |> Program.withErrorHandler (fun (_, ex) -> printfn $"Error: {ex.Message}")
+        |> Program.withConsoleTrace
+        |> Program.runView this view
         
     static member DesignVM = new MainViewModel()
