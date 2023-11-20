e9889d82d7be853db202a6367b3a816c4bc99051
Jordan Marr <jmarr@microdesk.com>
2023-11-20 12:45:34 -0500

    Updated xplat to latest gamma.4.

--- src/Samples/AvaloniaXPlatExample/AvaloniaXPlatExample/AppCompositionRoot.fs
+++ src/Samples/AvaloniaXPlatExample/AvaloniaXPlatExample/AppCompositionRoot.fs
@@ -13,12 +13,6 @@ type AppCompositionRoot() =
     let mainWindow = MainWindow()
 
     override this.RegisterServices services = 
-        // Find all IReactiveObject types in the assembly and add them as transient services.
-        let vmType = typeof<IReactiveObject>
-        this.GetType().Assembly.GetTypes()
-        |> Array.filter (fun t -> t.IsClass && not t.IsAbstract && t.GetInterfaces() |> Array.contains(vmType))
-        |> Array.iter (services.AddTransient >> ignore)
-        
         services.AddSingleton<FileService>(FileService(mainWindow))
 
     override this.RegisterViews() = 
