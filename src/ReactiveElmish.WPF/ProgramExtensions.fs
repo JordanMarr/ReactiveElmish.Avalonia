723519cc42717339d467c66c70b409d1e14b74a2
Jordan Marr <jmarr@microdesk.com>
2023-11-26 18:06:35 -0500

    Added ReactiveElmish.WPF project

---
+++ src/ReactiveElmish.WPF/ProgramExtensions.fs
@@ -0,0 +1,30 @@
+﻿namespace ReactiveElmish.WPF
+
+open Elmish
+open System
+open ReactiveElmish
+
+module Program =
+    /// Makes an Avalonia program via Program.mkProgram.
+    let mkAvaloniaProgram (init: unit -> 'Model * Cmd<'Msg>) update = 
+        Program.mkProgram init update (fun _ _ -> ())
+
+    /// Makes an Avalonia program via Program.mkSimple.
+    let mkAvaloniaSimple (init: unit -> 'Model) update =
+        Program.mkSimple init update (fun _ _ -> ())
+
+    /// Makes a reactive Elmish store from a program.
+    let mkStore (program: Program<unit, 'Model, 'Msg, unit>) = 
+        new WpfStore<'Model, 'Msg>(program) 
+        :> IStore<'Model, 'Msg>
+
+    /// Makes a reactive Elmish store from a program that terminates on the given "Terminate" 'Msg when the VM is disposed.
+    let mkStoreWithTerminate (vm: ReactiveElmishViewModel) (terminateMsg: 'Msg) (program: Program<unit, 'Model, 'Msg, unit>) = 
+        let prog = program |> Program.withTermination (fun m -> m = terminateMsg) (fun _ -> printfn $"Terminating store: dispatching {terminateMsg}")
+        let store = new WpfStore<'Model, 'Msg>(prog) :> IStore<'Model, 'Msg>
+        let disposable = 
+            { new IDisposable with 
+                member this.Dispose() = 
+                    store.Dispatch terminateMsg }
+        vm.AddDisposable disposable
+        store
---
+++ src/ReactiveElmish.WPF/WpfStore.fs
@@ -0,0 +1,28 @@
+﻿namespace ReactiveElmish.WPF
+
+open Elmish
+open ReactiveElmish
+open System.Windows
+open System.Windows.Threading
+open System.Threading
+
+type WpfStore<'Model, 'Msg> (program: Program<unit, 'Model, 'Msg, unit>) as this =
+    inherit ReactiveElmishStore<'Model, 'Msg>(program)
+
+    let defaultDispatcher _ = 
+        failwith "`Dispatch` failed because the Elmish loop has not been started."
+
+    do this.Dispatcher <- defaultDispatcher
+    do this.RunProgram(program)
+
+    /// Creates a WPF dispatcher that can be used to dispatch messages to the Elmish loop.
+    override this.WithDispatch (innerDispatch: Dispatch<'Msg>) = 
+        let dispatch msg = 
+            if Thread.CurrentThread = Application.Current.Dispatcher.Thread
+            then innerDispatch msg |> ignore
+            else Dispatcher.CurrentDispatcher.Invoke(fun () -> innerDispatch msg)
+        
+        this.Dispatcher <- dispatch
+        dispatch
+
+
