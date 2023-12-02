07b8f65de63962187e2d012bf6bd25dd6bcede66
Jordan Marr <jmarr@microdesk.com>
2023-12-02 17:10:24 -0500

    Moved core project back into the same repo.

--- src/publish.fsx
+++ src/publish.fsx
@@ -1,4 +1,4 @@
-#r "nuget: Fun.Build, 1.0.2"
+#r "nuget: Fun.Build, 1.0.5"
 
 open System.IO
 open System.Xml.Linq
@@ -9,14 +9,31 @@ let src = __SOURCE_DIRECTORY__
 pipeline "Publish" {
 
     stage "Build ReactiveElmish.Avalonia" {
-        run $"dotnet restore {src}/ReactiveElmish.Avalonia.sln"
-        run $"dotnet build {src}/ReactiveElmish.Avalonia.sln --configuration Release"
+        run $"dotnet restore {src}/ReactiveElmish.Avalonia/ReactiveElmish.Avalonia.fsproj"
+        run $"dotnet build {src}/ReactiveElmish.Avalonia/ReactiveElmish.Avalonia.fsproj --configuration Release"
+    }
+
+    stage "Publish ReactiveElmish" {
+        run (fun ctx ->             
+            let version = 
+                let project = FileInfo $"{src}/ReactiveElmish/ReactiveElmish.fsproj"
+                match XDocument.Load(project.FullName).Descendants("Version") |> Seq.tryHead with
+                | Some versionElement -> versionElement.Value
+                | None -> failwith $"Could not find a <Version> element in '{project.Name}'."
+            
+            let nugetKey = 
+                match ctx.TryGetEnvVar "REACTIVE_ELMISH_NUGET_KEY" with
+                | ValueSome nugetKey -> nugetKey
+                | ValueNone -> failwith "The NuGet API key must be set in an 'REACTIVE_ELMISH_NUGET_KEY' environmental variable"
+            
+            $"dotnet nuget push \"{src}/ReactiveElmish/bin/Release/ReactiveElmish.{version}.nupkg\" -s nuget.org -k {nugetKey} --skip-duplicate"
+        )
     }
     
     stage "Publish ReactiveElmish.Avalonia" {
         run (fun ctx ->             
             let version = 
-                let project = (FileInfo $"{src}/ReactiveElmish.Avalonia/ReactiveElmish.Avalonia.fsproj")
+                let project = FileInfo $"{src}/ReactiveElmish.Avalonia/ReactiveElmish.Avalonia.fsproj"
                 match XDocument.Load(project.FullName).Descendants("Version") |> Seq.tryHead with
                 | Some versionElement -> versionElement.Value
                 | None -> failwith $"Could not find a <Version> element in '{project.Name}'."
