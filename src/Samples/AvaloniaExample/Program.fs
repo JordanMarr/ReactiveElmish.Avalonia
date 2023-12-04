65bd17c168d553827155d37348cad19e5698e2b4
Jordan Marr <jmarr@microdesk.com>
2023-12-03 22:12:14 -0500

    Added icons to Todo sample

--- src/Samples/AvaloniaExample/Program.fs
+++ src/Samples/AvaloniaExample/Program.fs
@@ -4,11 +4,20 @@ open System
 open Avalonia
 open AvaloniaExample
 open Avalonia.ReactiveUI
+open Projektanker.Icons.Avalonia
+open Projektanker.Icons.Avalonia.MaterialDesign
+open Projektanker.Icons.Avalonia.FontAwesome
 
 module Program =
 
     [<CompiledName "BuildAvaloniaApp">] 
     let buildAvaloniaApp () = 
+
+        IconProvider.Current
+            .Register<MaterialDesignIconProvider>()
+            .Register<FontAwesomeIconProvider>()
+            |> ignore
+
         AppBuilder
             .Configure<App>()
             .UsePlatformDetect()
