50e8a92bfd2fccbe9d8a2c19afecc8d5a9e999ba
Jordan Marr <jmarr@microdesk.com>
2023-12-09 17:23:25 -0500

    Updated from dotnet template

--- src/Samples/AvaloniaXPlatExample/AvaloniaXPlatExample.Web/AppBundle/main.js
+++ src/Samples/AvaloniaXPlatExample/AvaloniaXPlatExample.Web/AppBundle/main.js
@@ -1,18 +1,13 @@
-import { dotnet } from "./dotnet.js";
-import { registerAvaloniaModule } from "./avalonia.js";
+﻿import { dotnet } from './dotnet.js'
 
 const is_browser = typeof window != "undefined";
 if (!is_browser) throw new Error(`Expected to be running in a browser`);
 
 const dotnetRuntime = await dotnet
-  .withDiagnosticTracing(false)
-  .withApplicationArgumentsFromQuery()
-  .create();
-
-await registerAvaloniaModule(dotnetRuntime);
+    .withDiagnosticTracing(false)
+    .withApplicationArgumentsFromQuery()
+    .create();
 
 const config = dotnetRuntime.getConfig();
 
-await dotnetRuntime.runMainAndExit(config.mainAssemblyName, [
-  window.location.search,
-]);
+await dotnetRuntime.runMainAndExit(config.mainAssemblyName, [window.location.search]);
--- src/Samples/AvaloniaXPlatExample/AvaloniaXPlatExample.Web/Program.fs
+++ src/Samples/AvaloniaXPlatExample/AvaloniaXPlatExample.Web/Program.fs
@@ -4,37 +4,45 @@ open Avalonia.Browser
 open Avalonia.ReactiveUI
 open AvaloniaXPlatExample
 
+open System.Runtime.Versioning
+open System.Threading.Tasks
+open Avalonia
+open Avalonia.Browser
+open Avalonia.ReactiveUI
+
 module Program =
     [<assembly: SupportedOSPlatform("browser")>]
     do
         ()
 
     [<CompiledName "BuildAvaloniaApp">]
-    let buildAvaloniaApp () =
-        AppBuilder
-            .Configure<App>()            
+    let buildAvaloniaApp () = AppBuilder.Configure<App>()            
 
     [<EntryPoint>]
     let main argv =
         // System.Diagnostics.Trace.Listeners.Add( new System.Diagnostics.ConsoleTraceListener()) |> ignore
         task { 
             try
-                let app = buildAvaloniaApp()
-                do! app.StartBrowserAppAsync("out")
-                app.UseReactiveUI() |> ignore
-                app.LogToTrace(
-                    level = Logging.LogEventLevel.Debug
-                        //areas = [|
-                        //    //Logging.LogArea.Binding
-                        //    //Logging.LogArea.Win32Platform
-                        //    //Logging.LogArea.Control
-                        //    //Logging.LogArea.Property
-                        //    //Logging.LogArea.Visual
-                        //    //Logging.LogArea.Animations
-                        //    //Logging.LogArea.Platform
-                        //|]
-                )
-                |> ignore
+                do! buildAvaloniaApp()
+                        .UseReactiveUI()
+                        .StartBrowserAppAsync("out")
+                    
+                //let app = buildAvaloniaApp()
+                //do! app.StartBrowserAppAsync("out")
+                //app.UseReactiveUI() |> ignore
+                //app.LogToTrace(
+                //    level = Logging.LogEventLevel.Debug
+                //        //areas = [|
+                //        //    //Logging.LogArea.Binding
+                //        //    //Logging.LogArea.Win32Platform
+                //        //    //Logging.LogArea.Control
+                //        //    //Logging.LogArea.Property
+                //        //    //Logging.LogArea.Visual
+                //        //    //Logging.LogArea.Animations
+                //        //    //Logging.LogArea.Platform
+                //        //|]
+                //)
+                //|> ignore
                 return 0
             with ex ->
                 printfn $"Exception: {ex.Message}\n{ex.StackTrace}"
