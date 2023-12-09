785bfc83f4567825f50da0dd489fc8ca2e0d3976
Darren Platt <github2@feb17.org>
2023-12-09 14:21:15 -0800

    MIN: debugging steps

--- src/Samples/AvaloniaXPlatExample/AvaloniaXPlatExample/App.axaml.fs
+++ src/Samples/AvaloniaXPlatExample/AvaloniaXPlatExample/App.axaml.fs
@@ -10,9 +10,12 @@ type App() =
 
     override this.Initialize() =
         // Initialize Avalonia controls from NuGet packages:
+        printfn "Initializing Avalonia"
         let _ = typeof<Avalonia.Controls.DataGrid>
+        printfn "loading Avalonia"
 
         AvaloniaXamlLoader.Load(this)
+        printfn "loaded - end of initialize"
 
     override this.OnFrameworkInitializationCompleted() =
         match this.ApplicationLifetime with
@@ -25,10 +28,13 @@ type App() =
             with x ->
                 printfn $"Exception: {x.Message} \n {x.StackTrace}"
         | :? ISingleViewApplicationLifetime as singleViewLifetime ->
+            printfn "OnFrameworkInitializationCompleted - ISingleViewApplicationLifetime"
             try
+                printfn "get mainview"
                 let view = MainView()
                 singleViewLifetime.MainView <- view
                 let x = ViewModels.MainViewModel.vm
+                printfn "start elmish loop"
                 x.StartElmishLoop(view)
             with x ->
                 printfn $"Exception: {x.Message} \n {x.StackTrace}"
